a4025a1212cec81fd8ae53d3c84e3a7f66a432ef
Zmy-Codes <91482717+Zmy-Shaurya@users.noreply.github.com>
2025-03-28 15:31:41 +0530

    Add files via upload
    A Simple Message SpamBot for Whatsapp and Telegram Desktop using pyautogui library.

---
+++ spambot2025.py
@@ -0,0 +1,19 @@
+import pyautogui as pyauto
+from time import sleep
+print("Spam Bot V2.\nGithub:@Zmy_Shaurya")
+
+String = input('Enter Your Message:')
+Reps = int(input('Enter No. of Repetitions: '))
+
+print('Open the chat window and place the cursor in message box.')
+print('Spamming in 10 seconds!')
+
+sleep(10)
+
+for i in range(n):
+    auto.write(s)
+    auto.press('enter')
+    sleep(0.01)
+
+print('\nThankyou For Using the Spambot!')
+print('Press ENTER to exit!')
